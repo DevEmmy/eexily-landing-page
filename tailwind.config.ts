893aab9781858c5f98dd2a49e68b259091cc5f65
Tinalexman <taiwoluwatobilobafestus@gmail.com>
2024-06-14 01:16:05 +0100

    Completed the get started page. Subject to later change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,10 @@ const config: Config = {
       colors: {
         primary: "#0054C2",
         secondary: "#FFE500",
-        input: "rgba(217, 217, 217, 0.3)"
+        input: "rgba(217, 217, 217, 0.3)",
+        tst1: "rgba(200, 224, 255, 0.3)",
+        tst2: "rgba(128, 255, 125, 0.3)",
+        tst3: "rgba(255, 247, 177, 0.4)"
       },
       boxShadow: {
         custom: "0 0 5px rgba(0, 0, 0, 0.1)"
