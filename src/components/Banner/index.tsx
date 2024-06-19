16b40dee1e007bd1833ffbeac48add4b3a7124eb
web-tranquility <eolaosebikan60@gmail.com>
2024-06-19 11:03:36 +0100

    fixed banner image

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -28,7 +28,7 @@ const Banner = () => {
       <img
         src="./girls-mobile.png"
         alt=""
-        className="absolute h-[245px] hidden md:block w-[80%] left-[10%] bottom-0  object-contain"
+        className="absolute h-[245px] hidden md:block w-[80%] left-[10%] bottom-0  object-cover"
       />
 
 
